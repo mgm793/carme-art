22555d3f431fb1c22cb45f0afe845d602c563f2f
Marc <mgm793@gmail.com>
2022-12-26 16:40:30 +0100

    refactor photozoom

--- components/PhotoCollection/PhotoZoom/PhotoZoom.js
+++ components/PhotoCollection/PhotoZoom/PhotoZoom.js
@@ -5,21 +5,23 @@ import styles from './PhotoZoom.module.css';
 
 export const PhotoZoom = ({image, title, price, onClose}) => {
   const {width, height} = useZoomSize(800, 10);
-  return <div className={styles.container} onClick={onClose}>
-   <div className={styles.imageContainer} onClick={(evt) => evt.stopPropagation()}>
-    <Image
-      src={image.url} 
-      alt={title} 
-      loader={({src}) => src} 
-      width={width} 
-      height={height} 
-      placeholder="blur" 
-      blurDataURL='/images/placeholder.webp'
-    />
-    <div className={styles.imageInfo}>
-      <h4>{title}</h4>
-      <p className={styles.imagePrice}>{price}€</p>
+  return (
+    <div className={styles.container} onClick={onClose}>
+    <div className={styles.imageContainer} onClick={(evt) => evt.stopPropagation()}>
+      <Image
+        src={image.url} 
+        alt={title} 
+        loader={({src}) => src} 
+        width={width} 
+        height={height} 
+        placeholder="blur" 
+        blurDataURL='/images/placeholder.webp'
+      />
+      <div className={styles.imageInfo}>
+        <h4>{title}</h4>
+        <p className={styles.imagePrice}>{price}€</p>
+      </div>
     </div>
-   </div>
-  </div>;
+    </div>
+  );
 };
